0a51b8cee049c4ca42b4789961f929faaff6bdd9
vortex204 <jespinal1193@gmail.com>
2023-05-22 14:30:35 -0400

    added select option by type

--- scripts/parks.js
+++ scripts/parks.js
@@ -13,20 +13,37 @@ function parkCard(item) {
 }
 
 function showCards(list, target) {
-  target.innerHTML = ""; //* clear
+  target.innerHTML = ""; //CLEAR
   list.forEach((item) => target.appendChild(parkCard(item)));
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   const results = document.getElementById("results");
   const select = document.getElementById("location");
+  const selectType = document.getElementById("type");
+
+  function applyFilters() {
+    const v = select.selectedOptions[0].value;
+    const matches = nationalParksArray.filter(
+      (item) => item.State == v || v == ""
+    );
+
+    const v2 = selectType.selectedOptions[0].value;
+    const matches2 = matches.filter(
+      (item) =>
+        item.LocationName.toLowerCase().includes(v2.toLowerCase()) || v2 == ""
+    );
+
+    showCards(matches2, results);
+  }
 
   locationsArray.forEach((item) => select.appendChild(locationOption(item)));
+  parkTypesArray.forEach((item) =>
+    selectType.appendChild(locationOption(item))
+  );
 
-  select.addEventListener("change", (e) => {
-    const v = select.selectedOptions[0].value;
-    const matches = nationalParksArray.filter((item) => item.State == v);
-    showCards(matches, results);
-  });
-  //* show all cards
+  select.addEventListener("change", applyFilters);
+  selectType.addEventListener("change", applyFilters);
+
+  //SHOW ALL CARDS
 });
